25ad3ed1146ea8d8c68d8cbf655416ce59ee8e27
happa <happaze@gmail.com>
2021-12-22 15:16:22 +0200

    Reverted 6c0fdf7

--- src/samples/sotest.py
+++
@@ -1,8 +0,0 @@
-from ctypes import *
-import pathlib
-
-libname = pathlib.Path().absolute() / "build/src/samples/libmjtools.so"
-lib = cdll.LoadLibrary(libname)
-print(lib.main())
-# for i in range(1,1000):
-#     print(lib.main())
